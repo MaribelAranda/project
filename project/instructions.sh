cf318e499db088d2b9ed498477792d769733e747
MaribelAranda <maria.aranda.guillen@stud.ki.se>
2017-02-16 18:37:14 +0100

    Add files via upload

---
+++ project/instructions.sh
@@ -0,0 +1,14 @@
+mkdir $1
+cd $1
+mkdir ./project1/
+mkdir ./datasets/
+mkdir ./bin/
+mkdir ./general_scripts/
+
+cd ./project1/
+mkdir ./scripts/ ./bash/
+mkdir ./bash/runall.sh
+mkdir ./bash/filter*sh
+mkdir ./input/ ./output/ ./logs/
+>readme.txt
+>commands.txt
